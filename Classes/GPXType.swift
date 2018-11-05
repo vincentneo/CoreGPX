e3f36cad3aa753a89515b79c2d555351b1c50620
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2018-11-05 20:18:14 +0800

    Update Classes/GPXType.swift

--- Classes/GPXType.swift
+++ Classes/GPXType.swift
@@ -71,6 +71,80 @@ class GPXType: NSObject {
         }
     }
     
+    func fix(value: String) -> GPXFix {
+        
+        if value == "2D" {
+            return .TwoDimensional
+        }
+        else if value == "3D" {
+            return .ThreeDimensional
+        }
+        else if value == "dgps" {
+            return .Dgps
+        }
+        else if value == "pps" {
+            return .Pps
+        }
+        else {
+            return .none
+        }
+        
+    }
     
+    func value(forFix fix: GPXFix) -> String {
+        switch fix {
+            case .none:             return "none"
+            case .TwoDimensional:   return "2D"
+            case .ThreeDimensional: return "3D"
+            case .Dgps:             return "dgps"
+            case .Pps:              return "pps"
+        }
+    }
+    
+    /*
+    func dateTime(value: String) -> Date {
+        var date: Date
+        let formatter = DateFormatter()
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        
+        // dateTime（YYYY-MM-DDThh:mm:ssZ)
+        formatter.dateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss'Z'"
+        //date = formatter.date(from: value ?? "")
+        if date != nil {
+            return date
+        }
+    }
+    */
+    
+    func value(forDateTime date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        
+        // dateTime（YYYY-MM-DDThh:mm:ssZ）
+        formatter.dateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss'Z'"
+        
+        return formatter.string(from: date)
+    }
+    
+    func nonNegativeIntFrom( string: String) -> Int {
+        if let i = Int(string) {
+            if i >= 0 {
+                return i
+            }
+        }
+        return 0
+    }
+    
+    func value(forNonNegativeInt int: Int) -> String {
+        if int >= 0 {
+            return String(format: "%ld", int)
+        }
+        return "0"
+    }
 
+}
+
+enum GPXFix: Int {
+    case none = 0
+    case TwoDimensional, ThreeDimensional, Dgps, Pps
 }
