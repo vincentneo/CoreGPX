74c3810de85a3e2057d52e1ed24f32dd230d2eda
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2018-11-22 18:38:32 +0800

    full port of GPXRoutePoint

--- Classes/GPXRoutePoint.swift
+++ Classes/GPXRoutePoint.swift
@@ -11,5 +11,17 @@ class GPXRoutePoint: GPXWaypoint {
     
     // MARK:- Instance
     
+    func routePoint(with latitude: CGFloat, longitude: CGFloat) -> GPXRoutePoint {
+        let routePoint = GPXRoutePoint()
+        routePoint.latitude = latitude
+        routePoint.longitude = longitude
+        
+        return routePoint
+    }
     
+    // MARK:- Tag
+    
+    override func tagName() -> String! {
+        return "rtept"
+    }
 }
