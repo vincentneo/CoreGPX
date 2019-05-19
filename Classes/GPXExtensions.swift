7bb95ed5b12c8bb95195937c3a40c6af0ee5508b
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2019-05-19 15:39:36 +0800

    Support for creation with GPXExtensions

--- Classes/GPXExtensions.swift
+++ Classes/GPXExtensions.swift
@@ -27,6 +27,17 @@ open class GPXExtensions: GPXElement, Codable {
         return "extensions"
     }
     
+    public init(custom: [[String : String]]) {
+        self.attributes = custom
+    }
+    
+    public func insertParentTag(at index: Int, tagName: String) {
+        if !attributes.indices.contains(index) {
+            attributes.append([String : String]())
+        }
+        self.attributes[index][tagName] = "index \(index)"
+    }
+    
     init(dictionary: [String : String]) {
 
         for key in dictionary.keys {
@@ -40,9 +51,50 @@ open class GPXExtensions: GPXElement, Codable {
         }
         
     }
+    private var hasParentTag = false
+    func addChildTag(fromIndexOfAttributes index: Int) {
+        if attributes[index].values.contains("index \(index)") {
+            
+        }
+    }
     
     // MARK:- GPX
     override func addChildTag(toGPX gpx: NSMutableString, indentationLevel: Int) {
         super.addChildTag(toGPX: gpx, indentationLevel: indentationLevel)
+        
+        var hasParentElement = false
+        var parentKey = String()
+        var newIndentationLevel = indentationLevel
+        var index = Int()
+        
+        for attribute in attributes {
+            if attribute.values.contains("index \(index)") {
+                for key in attribute.keys {
+                    if attribute[key] == "index \(index)" {
+                        newIndentationLevel += 1
+                        gpx.append(String(format: "%@<%@>\r\n", indent(forIndentationLevel: newIndentationLevel), key))
+                        parentKey = key
+                        hasParentElement = true
+                    }
+                }
+            }
+            
+            for key in attribute.keys {
+                if attribute[key] != "index \(index)" {
+                    gpx.appendFormat("%@<%@>%@</%@>\r\n", indent(forIndentationLevel: newIndentationLevel + 1), key, attribute[key] ?? "", key)
+                }
+            }
+            
+            if hasParentElement {
+                gpx.append(String(format: "%@</%@>\r\n", indent(forIndentationLevel: newIndentationLevel), parentKey))
+                hasParentElement = false
+            }
+            // increment index
+            index += 1
+            
+            //reset
+            newIndentationLevel = indentationLevel
+        }
+        
     }
 }
